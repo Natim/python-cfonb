d11df934f603bcb56f1f6182e35bbcac82d936d7
Hong-Thai Nguyen <hnguyen@dhatim.com>
2021-04-22 10:50:34 +0200

    fix setup files

--- setup.py
+++ setup.py
@@ -1,29 +1,47 @@
-from setuptools import setup, find_packages
+"""A setuptools based setup module.
 
-version = '2.9'
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
 
-long_description = open('README.rst').read()
+# To use a consistent encoding
+from codecs import open
+from os import path, environ
+
+# Always prefer setuptools over distutils
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
       name='python-cfonb',
-      version=version,
+      
+      version='2.9',
+      
       description="Pure Python lib to read/write CFONB files, forked from https://github.com/akretion/python-cfonb",
       long_description=long_description,
+      
       classifiers=[],
       keywords=['cfonb', 'bank', 'statement', 'parser'],
+      
       author='Dhatim',
       author_email='contact@dhatim.com',
+      
       url='https://github.com/dhatim/python-cfonb',
-      download_url = 'https://github.com/dhatim/python-cfonb/archive/2.9.tar.gz',
+      
       license='LGPL',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      test_suite = "cfonb.tests.test_all.suite"
-      )
+      packages=['python-cfonb'],
+      python_requires='>=2.7',
+	  install_requires=['semantic_version>=2.7.0', 'toml'],
+      
+      entry_points={
+        'console_scripts': [
+            'python-cfonb=python-cfonb.command_line:main'
+        ],
+    },
+)
